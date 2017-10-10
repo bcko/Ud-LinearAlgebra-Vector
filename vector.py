9ab72270b15e84bed92b37dc0c05ad2230305326
bcko <gobcko@gmail.com>
2017-10-10 14:43:19 -0400

    angle calculation, better precision with Decimal package

--- vector.py
+++ vector.py
@@ -1,11 +1,14 @@
-from math import sqrt
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
 
 class Vector(object):
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -33,13 +36,32 @@ class Vector(object):
     def normalized(self):
         try:
             magnitude = self.magnitude()
-            return self.times_scalar(1./magnitude)
+            return self.times_scalar(Decimal('1.0')/magnitude)
 
         except ZeroDivisionError:
             raise Exception('Cannot normalize the zero vector')
 
     def dot(self, v):
         return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+
+
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = acos(u1.dot(u2))
+
+            if in_degrees:
+                degrees_per_radian = 180./pi
+                return angle_in_radians * degrees_per_radian
+            else:
+                return angle_in_radians
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot compute an angle with the zero vector')
+            else:
+                raise e
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
